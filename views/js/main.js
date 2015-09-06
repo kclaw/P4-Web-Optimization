5aa2abf328f6797a4ae0cc82568f825690919f4e
kwanc law <kwanc.law@gmail.com>
2015-09-07 03:05:40 +0800

    edit on changePizzaSizes function according to requirement of completion in 5ms(new one with test in 0.5ms); delete determineDx function because of not use in case.

--- views/js/main.js
+++ views/js/main.js
@@ -424,10 +424,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var elements = document.querySelectorAll(".randomPizzaContainer");
+    //change each pizza container to a new width by percentage
+    var newsize = size=='1'?'25%':size=='2'?'33.33%':size=='3'?'50%':null;
+    for(var i=0,len=elements.length;i<len;i++){
+        elements[i].style.width = newsize;
     }
   }
 
